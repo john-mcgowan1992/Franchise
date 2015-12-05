ddffb1f34553fd9e54d6214e31d083bd82376d75
john-mcgowan1992 <mcgowanj92@gmail.com>
2015-12-05 15:35:04 -0500

    added front page

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 module.exports = router;
 
-router.use('/artists', require('./artists'))
-router.use('/albums', require('./albums'))
-router.use('/playlists', require('./playlists'))
-router.use('/songs', require('./songs'))
 
 // Make sure this is after all of
 // the registered routes!
